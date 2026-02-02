770639b211b75aa1ba4f218224998017bec35414
jaco0010 <jacovillegas12@gmail.com>
2026-02-02 15:22:54 -0500

    Add files via upload

---
+++ heladeria/src/main/java/com/jaco/heladeria/HeladeriaApplication.java
@@ -0,0 +1,16 @@
+package com.jaco.heladeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeladeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeladeriaApplication.class, args);
+	}
+
+}
+
+
+//NO TOUCH PLEASE
---
+++ heladeria/src/test/java/com/jaco/heladeria/HeladeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaco.heladeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeladeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
